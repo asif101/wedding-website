48edc31419f49ff14bd33b792950642b402c30f5
Asif Rahman <asifrahman8@gmail.com>
2022-04-15 22:14:57 -0400

    fixing language in picnic section

--- src/components/Picnic/Picnic.js
+++ src/components/Picnic/Picnic.js
@@ -10,7 +10,7 @@ export default class Picnic extends React.Component {
                    
                     <div className='text-box'>
                         <h2>Picnic</h2>
-                        <p>For those sticking around, we invite you to join us on Sunday (May 8) for snacks and yard games in the Front Royal Area</p>
+                        <p>For those sticking around, we invite you to join us on Sunday (May 8) for snacks and yard games at a park in the Front Royal Area</p>
                         <p>Start time: 11:30am</p>
                         <p>Exact location at a park TBD</p>
                     </div>
